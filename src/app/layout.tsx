db3f94b3304a974918609fc151a0be26311879ce
Your Name <you@example.com>
2024-06-06 01:19:44 +0200

    Init + Splash screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import '@radix-ui/themes/styles.css';
+const montserrat = Montserrat({ subsets: ["latin"] });
+import { Theme } from '@radix-ui/themes';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>
+        <Theme accentColor="lime" radius="large" appearance="dark">
+          {children}
+        </Theme>
+      </body>
     </html>
   );
 }
