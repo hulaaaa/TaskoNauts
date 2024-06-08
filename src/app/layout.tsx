d27d1e52f9ad0907ac99a7c835477d27535b649e
Your Name <you@example.com>
2024-06-08 17:55:37 +0200

    Metadata 3v

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,8 @@ export const metadata: Metadata = {
     icon: '/favicon-32x32.png'
   },
   openGraph: {
+    title: "TaskoNauts",
+    description: "Start working in the most comfortable and modern team and task management environment",
     images: 'https://photos.sphereshowcase.com/tBJczsgyzUAP3woETDr31.jpg',
   },
 };
@@ -24,6 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons.icon} type="image/png" />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:image" content={metadata.openGraph.images} />
+        <meta property="og:type" content="website" />
+      </Head>
       <body className={montserrat.className}>
         <Theme accentColor="gray" radius="large" appearance="dark">
           <FlareCursor />
