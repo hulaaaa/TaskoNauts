35b82d9cc4e60c7653db876d6fdac368a5563302
Your Name <you@example.com>
2024-06-08 19:33:16 +0200

    switch admin

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,9 +2,12 @@
 import { Label } from "../../../components/ui/label";
 import { Input } from "../../../components/ui/input";
 import { cn } from "../../../components/Spotlight/cn";
+import { motion } from 'framer-motion';
+
 import {
   IconBrandGithub,
   IconBrandGoogle,
+  IconSwitch2,
 } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
 import { AuthLoader as Loader } from "../../dashboard/ui/loaderAuth";
@@ -28,28 +31,64 @@ const loadingStates = [
 export default function Signup() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
-
+  const [isAdmin, setIsAdmin] = useState(false);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
     router.prefetch('/dashboard'); 
     setTimeout(() => {
-      router.push('/dashboard'); // Navigate to the dashboard
+      router.push('/dashboard'); 
     }, loadingStates.length * 1500);
   };
+  useEffect(() => {
+    if(isAdmin==true){
+
+    }else{
+
+    }
+  }, [isAdmin]);
 
   return (
     <div className="container">
       <div
         className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black"
-        
       >
-        <h2 className="font-bold text-xl text-white">
-          Welcome to TaskoNauts
-        </h2>
-        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Sign up and start building a team
-        </p>
+        <div className="flex flex-row justify-between items-center">
+          <div>
+            <h2 className="font-bold text-xl text-white">
+              {!isAdmin ? "Welcome to TaskoNauts" : "Welcome to Panel TaskoNauts"}
+            </h2>
+            <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+              {isAdmin ? "Sign up and start building a team" : "Sign up and start managing your tasks"}
+            </p>
+          </div>
+          <button
+            onClick={() => setIsAdmin(!isAdmin)}
+            data-cursor-pointer
+            className="overflow"
+          >
+            <motion.div
+              initial={{ x: 0 }}
+              whileHover={{ x: -5 }}
+              transition={{ type: 'spring', stiffness: 300 }}
+              className={
+                !isAdmin ?
+                "flex items-center gap-1 text-lg font-semibold bg-transparent border-none px-4 overflow-hidden" :
+                "flex items-center gap-1 text-lg font-semibold bg-gradient-to-br group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 px-4 py-1 text-white rounded-md font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] overflow-hidden"
+              }
+              data-cursor-pointer
+            >
+              <div className="relative flex flex-row gap-1">
+                <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+                <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+                <p data-cursor-pointer className="text-neutral-600 text-sm max-w-sm dark:text-neutral-300">
+                  /admin
+                </p>
+                <IconSwitch2 data-cursor-pointer />
+              </div>
+            </motion.div>
+          </button>
+        </div>
 
         <form className="my-8" onSubmit={handleSubmit}>
           <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
